062764dd817fc1117de1e660f2e49436c8910d95
Josh Brown <josh@epidev.com>
2021-12-21 21:32:20 +1100

    Send notification to slack for each dns req received!

--- src/src/Lib.hs
+++ src/src/Lib.hs
@@ -6,7 +6,9 @@ import Control.Concurrent (forkFinally)
 import qualified Control.Exception as E
 import Control.Monad (unless, forever, void)
 import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S8
 import           Data.ByteString (ByteString)
+import qualified Data.Text as T
 import Network.Socket
 import Network.Socket.ByteString (recv, sendAll)
 import qualified Network.DNS.IO as DNS.IO
@@ -15,21 +17,25 @@ import           Network.DNS.Types (DNSMessage)
 import qualified Network.DNS.Encode as DNS.Encode
 import qualified Network.DNS.Decode as DNS.Decode
 import Data.IP.Internal
+import qualified SlackHook as Slack
 
 main :: IO ()
-main = runUDPServer Nothing "53" talk
+main = do
+  slackHook <- readFile "config" >>= return . T.unpack . T.strip . T.pack
+  runServer slackHook
+
+runServer :: String -> IO ()
+runServer slackHook = do
+  runUDPServer Nothing "53" talk
   where
     talk s msg client  = do
         putStrLn $ show msg
         mapM_
-          (\q ->
+          (\q -> do
+            Slack.push slackHook (S8.unpack $ DNS.qname q)
             DNS.IO.sendTo s
               (DNS.Encode.encode
-                (DNS.IO.responseA
-                  (DNS.identifier $ DNS.header msg)
-                  q
-                  [read "127.0.0.1" :: IPv4]
-                )
+                (DNS.IO.responseA (DNS.identifier $ DNS.header msg) q [])
               )
               client
           )
---
+++ src/src/SlackHook.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+module SlackHook
+  (push) where
+
+import Control.Monad.IO.Class
+import Data.Aeson
+import Network.HTTP.Req
+import Text.URI
+import Data.Text
+
+push :: String -> String -> IO ()
+push hookUri domain = do
+  let payload =
+        object
+          [ "text" .= ("RECVD `" ++ domain ++ "`" :: String)
+          , "username" .= ("DNS Flare" :: String)
+          ]
+  uri <- mkURI $ Data.Text.pack hookUri
+  case useHttpsURI uri of
+    Nothing -> do
+      putStrLn "Failed to parse URI"
+      return ()
+    Just (url, option) -> runReq defaultHttpConfig $ do
+      r <-
+        req
+          POST
+          url
+          (ReqBodyJson payload)
+          ignoreResponse
+          mempty
+      liftIO $ case responseStatusCode r of
+        200 ->
+          return ()
+        x ->
+          putStrLn $ "Slack Failed with status code " ++ show x
